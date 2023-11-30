65347e668c61e572e95ffc6dcacaefb341775114
zipianli818 <100552310+zipianli818@users.noreply.github.com>
2023-11-30 16:50:03 +1100

    add view.Weatherhtml

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const addCityButton = document.getElementById("addCityButton");
 addCityButton.addEventListener('click', ()=> {
     window.location.href = "searchPage.html";
 });
+
+viewCityButton.viewEventListen('click', ()=> {
+    window.location.href = "viewWeather.html";
+})
